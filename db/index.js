191df44c17d8caf8bb60edc6dbc7c53f6a0785ce
Ruben Diaz <rubendb95@gmail.com>
2024-08-17 13:45:41 +0200

    created routes index.js, user.routes.js, devices.routes.js

--- db/index.js
+++ db/index.js
@@ -6,8 +6,10 @@ const MONGO_URI =
 mongoose
   .connect(MONGO_URI)
   .then((x) => {
+
     const dbName = x.connections[0].name
     console.log(`Connected to Mongo! Database name: "${dbName}"`)
+
   })
   .catch((err) => {
     console.error("Error connecting to mongo: ", err)
---
+++ routes/index.js
@@ -0,0 +1,4 @@
+module.exports = app => {
+    const deviceRouter = require("./devices.routes")
+    const usersRouter = require("./users.routes")
+}
