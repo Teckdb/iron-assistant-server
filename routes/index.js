6f4033f8af99e0bee9c184f65bf261ceeec904b9
Robert-caridad <robertcaridad@gmail.com>
2024-08-17 18:01:14 +0200

    added areas routes

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,6 @@ module.exports = app => {
     const deviceRouter = require("./devices.routes")
     const usersRouter = require("./users.routes")
     const areasRouter = require("./areas.routes")
+    app.use("/api/areas", areasRouter)
+
 }
