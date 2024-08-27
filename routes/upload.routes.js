18e5151f4c39f640f97fb0246b1b2863490a4985
Ruben Diaz <rubendb95@gmail.com>
2024-08-27 22:56:27 +0200

    update

--- routes/upload.routes.js
+++ routes/upload.routes.js
@@ -1,22 +1,16 @@
 const router = require("express").Router()
-const isAuthenticated = require("../middleware/verifyToken")
 
-const cloudinary = require("cloudinary").v2;
+const uploader = require('./../middleware/uploader.middleware')
 
+router.post('/image', uploader.single('imageData'), (req, res) => {
 
-router.post("/images", isAuthenticated,
-
-    getAllUsers = (req, res, next) => {
-        User
-            .find()
-            .select({ email: 1 })
-            .sort({ email: 1 })
-            .then(response => res.json(response))
-            .catch(err => next(err))
+    if (!req.file) {
+        res.status(500).json({ errorMessage: 'Error caragndo el archivo' })
+        return
     }
 
-)
-
+    res.json({ cloudinary_url: req.file.path })
+})
 
 
 module.exports = router
